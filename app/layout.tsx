ff1b0f3aab36740af0d8caa26224fee5c61a8b05
KalanNet <nicolastynovskyi@gmail.com>
2025-11-01 14:22:24 -0600

    Update: layout, main page, and add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
       "Empowering seniors 50+ to live their best lives through resources, directories and community programs.",
     images: ["/og.jpg"],
   },
-  icons: { icon: "/LogoLoader.png" },
+  icons: { icon: "/favicon.ico" },
 };
 
 /* ---------- Viewport / theme ---------- */
