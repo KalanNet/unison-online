679fa308189ece392d324fcd1dd867a757444897
KalanNet <nicolastynovskyi@gmail.com>
2025-11-02 12:32:01 -0700

    Fix: update globals.css and PublicViewer.tsx

--- app/public/PublicViewer.tsx
+++ app/public/PublicViewer.tsx
@@ -85,8 +85,8 @@ export default function PublicViewer({
             justifyContent: "center",
             width: ctrl.single ? Math.round(ctrl.baseSize.w * ctrl.fitScale) : Math.round(ctrl.baseSize.w * ctrl.fitScale * 2),
             height: Math.round(ctrl.baseSize.h * ctrl.fitScale),
-            maxWidth: "100vw",
-            maxHeight: "100vh",
+            maxWidth: "100%",
+            maxHeight: "100%",
             position: "relative",
           }}
         >
