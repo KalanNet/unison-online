3d357a13a32342a1f58fa7efe54b020b46ff1df8
KalanNet <nicolastynovskyi@gmail.com>
2025-11-09 14:18:00 -0700

    Add public directory head file

---
+++ app/(public)/directory/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      {/* Ресурсні підказки ТІЛЬКИ для /directory/* */}
+      <link rel="dns-prefetch" href="https://cdn.unisonalberta.online" />
+      <link rel="preconnect" href="https://cdn.unisonalberta.online" crossOrigin="" />
+    </>
+  );
+}
